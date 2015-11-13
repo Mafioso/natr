457a2032dfd5cc1cf640182b3926a59a80276057
Rustem <r.kamun@gmail.com>
2015-11-13 14:50:37 +0600

    testing rest

--- test/test_dummy.py
+++ test/test_dummy.py
@@ -1,5 +1,8 @@
+import json
 import unittest2 as unittest
 from faker import Factory
+from rest_framework.test import APITestCase
+from rest_framework import status
 
 fake = Factory.create('ru_RU')
 
@@ -16,3 +19,14 @@ class ContactListTest(unittest.TestCase):
 	@classmethod
 	def tearDownClass(cls):
 		pass
+
+
+class DummyApiTestCase(APITestCase):
+
+	def test_feed(self):
+		response = self.client.get('/dummies/feed/')
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		content = json.loads(response.content)
+		self.assertTrue(isinstance(content, list))
+		self.assertTrue(content[0].has_key('dummy_1'))
+		self.assertTrue(content[0].has_key('dummy_2'))
