d299424e53936e4896fab7be0c0b3c19618677e6
Rustem <r.kamun@gmail.com>
2015-12-15 02:13:40 +0600

    remove all migrations

--- apps/documents/utils.py
+++ apps/documents/utils.py
@@ -3,12 +3,12 @@
 import models as doc_models
 
 def get_default_gp_type():
-	def_gp_doc_types = doc_models.GPDocumentType.objects.filter(name=u"договор")
+	doc_types = doc_models.GPDocumentType.objects.filter(name=u"договор")
 
-	if len(def_gp_doc_types) == 0:
+	if len(doc_types) == 0:
 		doc_models.GPDocumentType.create_default()
-		def_gp_doc_types = doc_models.GPDocumentType.objects.filter(name=u"договор")
+		doc_types = doc_models.GPDocumentType.objects.filter(name=u"договор")
 
-	return def_gp_doc_types[0]
+	return doc_types[0]
 
 	 
