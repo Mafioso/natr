3d2d1620d491fbb70fa67ec3838dbfea45018eb8
Abyken Nurlan <abyken.nurlan@gmail.com>
2015-12-26 14:05:03 +0600

    merge migration

---
+++ apps/projects/migrations/0010_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('projects', '0009_merge'),
+        ('projects', '0009_auto_20151226_0803'),
+    ]
+
+    operations = [
+    ]
