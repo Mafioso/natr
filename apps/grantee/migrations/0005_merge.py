0dadfcf57402c9b81f5a685e3f10c6c2e1e1f7fc
Rustem <r.kamun@gmail.com>
2015-12-27 21:59:23 +0600

    merge

---
+++ apps/grantee/migrations/0005_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('grantee', '0004_auto_20151227_1348'),
+        ('grantee', '0004_contactdetails_grantee'),
+    ]
+
+    operations = [
+    ]
