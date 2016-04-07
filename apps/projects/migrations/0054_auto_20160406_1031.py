5c5e2a19faa312a2184b6ed2405bed3c28d995ea
Sattar Stamkulov <sattar.coder@gmail.com>
2016-04-07 12:38:33 +0600

    added: management command to fill admin group with permissions; fixed: incorrect settings was preventing start

---
+++ apps/projects/migrations/0054_auto_20160406_1031.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('projects', '0053_order_cost_type'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='riskdefinition',
+            options={'verbose_name': '\u0412\u0438\u0434\u044b \u0440\u0438\u0441\u043a\u043e\u0432'},
+        ),
+    ]
