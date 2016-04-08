f066c77d97ad17329dbbe294abe32cf7ada96f22
Sattar Stamkulov <sattar.coder@gmail.com>
2016-04-08 13:22:53 +0600

    added: auto add perms into admin group on startup

--- apps/auth2/management/__init__.py
+++ apps/auth2/management/__init__.py
@@ -7,7 +7,11 @@ import auth2.models
 
 def create_default_groups(sender, **kwargs):
 	for def_group in NatrGroup.ROLES:
-		NatrGroup.objects.get_or_create(name=def_group)
+		group = NatrGroup.objects.get_or_create(name=def_group)[0]
+		if def_group == NatrGroup.ADMIN:
+			group.permissions = Permission.objects.all()
+			group.save()
+			print '%s permissions added into admin group' % group.permissions.count()
 		print 'GROUP (NatrGroup) created %s' % def_group
 
 
@@ -15,22 +19,22 @@ post_syncdb.connect(create_default_groups, sender=auth2.models)
 
 
 def add_view_permissions(sender, **kwargs):
-    """
-    This syncdb hooks takes care of adding a view permission too all our
-    content types.
-    """
-    # for each of our content types
-    for content_type in ContentType.objects.all():
-        # build our permission slug
-        codename = "view_%s" % content_type.model
+	"""
+	This syncdb hooks takes care of adding a view permission too all our
+	content types.
+	"""
+	# for each of our content types
+	for content_type in ContentType.objects.all():
+		# build our permission slug
+		codename = "view_%s" % content_type.model
 
-        # if it doesn't exist..
-        if not Permission.objects.filter(content_type=content_type, codename=codename):
-            # add it
-            Permission.objects.create(content_type=content_type,
-                                      codename=codename,
-                                      name="Can view %s" % content_type.name)
-            # print "Added view permission for %s" % content_type.name
+		# if it doesn't exist..
+		if not Permission.objects.filter(content_type=content_type, codename=codename):
+			# add it
+			Permission.objects.create(content_type=content_type,
+									  codename=codename,
+									  name="Can view %s" % content_type.name)
+			# print "Added view permission for %s" % content_type.name
 
 # check for all our view permissions after a syncdb
 post_syncdb.connect(add_view_permissions)
