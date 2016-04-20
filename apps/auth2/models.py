bedd75c5ad180e8a380c435c4eb2d6bf2640013c
Abyken Nurlan <abyken.nurlan@gmail.com>
2016-04-20 11:01:00 +0600

    fix

--- apps/auth2/models.py
+++ apps/auth2/models.py
@@ -199,7 +199,7 @@ def assign_user_group(sender, instance, created=False, **kwargs):
     """If natr user does not belong to any group, assign expert by default."""
     if not created:
         return
-    if instance.is_expert() or instance.is_manager() or instance.is_risk_expert():
+    if instance.is_expert() or instance.is_manager() or instance.is_risk_expert() or instance.is_director():
         return
     instance.add_to_experts()
 
